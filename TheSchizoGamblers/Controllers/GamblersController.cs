a096278f326a8928644cd6a4b5f2b0cbf7bfb898
Diansavov <diansavov2@abv.bg>
2023-11-30 17:28:42 +0200

    Add project files.

---
+++ TheSchizoGamblers/Controllers/GamblersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheSchizoGamblers.Models;
+
+namespace TheSchizoGamblers.Controllers
+{
+    public class GamblersController : Controller
+    {
+        public IActionResult Gamblers()
+        {
+            GamblersModel gamblersModel = new GamblersModel("Gunter");
+            return View(gamblersModel);
+        }
+    }
+}
---
+++ TheSchizoGamblers/Models/GamblersModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TheSchizoGamblers.Models
+{
+    public class GamblersModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+
+        public GamblersModel(string username)
+        {
+            this.Username = username;
+        }
+    }
+}
