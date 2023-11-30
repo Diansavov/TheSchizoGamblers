c28a269eee71828b1dd1d114f8509542e2a23327
Diansavov <diansavov2@abv.bg>
2023-11-30 21:23:15 +0200

    LogInSuccess

--- TheSchizoGamblers/Controllers/GamblersController.cs
+++ TheSchizoGamblers/Controllers/GamblersController.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TheSchizoGamblers.Data;
 using TheSchizoGamblers.Models;
+using TheSchizoGamblers.Models.ViewModels;
 
 namespace TheSchizoGamblers.Controllers
 {
     public class GamblersController : Controller
     {
+        private readonly GamblersContext gamblersContext;
+
+        public GamblersController(GamblersContext gamblersContext)
+        {
+            this.gamblersContext = gamblersContext;
+        }
+
+        [HttpGet]
         public IActionResult Gamblers()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Gamblers(GamblersViewModel addGamblerRequest)
+        {
+            GamblersModel gambler = new GamblersModel()
+            {
+                ID = Guid.NewGuid(),
+                Username = addGamblerRequest.Username,
+                Email = addGamblerRequest.Email,
+                Password = addGamblerRequest.Password,
+                DateOfBirth = addGamblerRequest.DateOfBirth
+            };
+            await gamblersContext.Gamblers.AddAsync(gambler);
+            await gamblersContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ TheSchizoGamblers/Models/ViewModels/GamblersViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TheSchizoGamblers.Models.ViewModels
+{
+    public class GamblersViewModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
