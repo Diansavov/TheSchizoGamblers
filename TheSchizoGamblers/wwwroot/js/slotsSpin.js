a309d281e2cf5be65691adbc1cc1af68776c5ce6
Diansavov <diansavov2@abv.bg>
2024-02-23 21:46:59 +0200

    BasicSpinLogic

--- TheSchizoGamblers/wwwroot/js/slotsSpin.js
+++ TheSchizoGamblers/wwwroot/js/slotsSpin.js
@@ -1,15 +1,28 @@
 const btn = document.getElementById("spinButton");
 
+function spinSlots(imges, bottomImges) {
+
+    for (let i = 0; i < imges.length; i++) {
+        if (i > 9) {
+            bottomImges[i].style.transform = "translateY(180px)";
+            bottomImges[i].style.transition = "transform 500ms ease-in-out";
+        }
+        else {
+            imges[i].style.transform = "translateY(180px)";
+            imges[i].style.transition = "transform 500ms ease-in-out";
+        }
+    }
+}
+
 btn.onclick = () => {
-    const imges = document.querySelectorAll("[id=slotsFigure]");
-    
-    imges.forEach(element => {
-        element.style.transform = "translateY(180px)";
-        element.style.transition = "transform 500ms ease-in-out";
-    });
+    const imges = document.querySelectorAll("[class=slots-image]");
+    const bottomImges = document.querySelectorAll("[id=slotsFigure]");
+
+    spinSlots(imges, bottomImges);
+
     setTimeout(() => {
         document.getElementById("slotForm").submit();
     }, 600);
+
     return false;
 }
-
