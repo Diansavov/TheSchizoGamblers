69f343e77fef4b5be5f6d9e201ecc1519be3ec7b
Diansavov <diansavov2@abv.bg>
2024-02-23 23:42:19 +0200

    CleanedLogicButNotDone

--- TheSchizoGamblers/wwwroot/js/slotsSpin.js
+++ TheSchizoGamblers/wwwroot/js/slotsSpin.js
@@ -1,28 +1,52 @@
 const btn = document.getElementById("spinButton");
 
-function spinSlots(imges, bottomImges) {
+function spinSlots(matrixImages, matrixBottomImages, currentIteration) {
+    
+    for (let i = 0; i < matrixImages.length; i++) {        
+        if (i == currentIteration) {
+            for (let j = 0; j < matrixImages[i].length; j++) {
 
-    for (let i = 0; i < imges.length; i++) {
-        if (i > 9) {
-            bottomImges[i].style.transform = "translateY(180px)";
-            bottomImges[i].style.transition = "transform 500ms ease-in-out";
+                matrixBottomImages[i][j].style.transform = "translateY(180px)";
+                matrixBottomImages[i][j].style.transition = "transform 500ms ease-in-out";
+            } 
         }
-        else {
-            imges[i].style.transform = "translateY(180px)";
-            imges[i].style.transition = "transform 500ms ease-in-out";
+        else{
+            for (let j = 0; j < matrixImages[i].length; j++) {
+
+                matrixImages[i][j].style.transform = "translateY(180px)";
+                matrixImages[i][j].style.transition = "transform 500ms ease-in-out";
+            }
         }
     }
+    setTimeout(() => {
+        spinSlots(matrixImages, matrixBottomImages, currentIteration - 1);
+    }, 500);
+
 }
 
 btn.onclick = () => {
+    const currentIteration = 2;
+    let currentIndex = 0;
+
     const imges = document.querySelectorAll("[class=slots-image]");
     const bottomImges = document.querySelectorAll("[id=slotsFigure]");
+    
+    const matrixImages = [[], [], []];
+    const matrixBottomImages = [[], [], []];
 
-    spinSlots(imges, bottomImges);
+    for (let i = 0; i < matrixImages.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            matrixImages[i][j] = imges[currentIndex];
+            matrixBottomImages[i][j] = bottomImges[currentIndex];
+            currentIndex++;
+        }
+    }
+
+    spinSlots(matrixImages, matrixBottomImages, currentIteration);
 
     setTimeout(() => {
         document.getElementById("slotForm").submit();
-    }, 600);
+    }, 1600);
 
     return false;
 }
