a098806b16b497e58edcbf71a900b4823254d14e
Diansavov <diansavov2@abv.bg>
2024-02-25 12:44:00 +0200

    JsAnimationWorks

--- TheSchizoGamblers/wwwroot/js/slotsSpin.js
+++ TheSchizoGamblers/wwwroot/js/slotsSpin.js
@@ -1,36 +1,11 @@
 const btn = document.getElementById("spinButton");
+const currentIteration = 2;
+const flippedIteration = 1;
+let currentIndex = 0;
+const [matrixImages, matrixBottomImages] = FillSlotsMatrix();
+const spinLenght = 250;
 
-function spinSlots(matrixImages, matrixBottomImages, currentIteration, flippedIteration) {
-
-    for (let i = 0; i < matrixImages.length - flippedIteration; i++) {
-        for (let j = 0; j < matrixImages[i].length; j++) {
-
-            matrixImages[i][j].style.transform = `translateY(${180 * flippedIteration}px)`;
-            matrixImages[i][j].style.transition = "transform 500ms ease-in-out";
-
-            matrixBottomImages[currentIteration][j].style.transform = "translateY(180px)";
-            matrixBottomImages[currentIteration][j].style.transition = "transform 500ms ease-in-out";
-        }
-    }
-
-    if (flippedIteration == matrixImages.length) {
-        for (let j = 0; j < matrixImages[0].length; j++) {
-            matrixBottomImages[currentIteration][j].style.transform = "translateY(180px)";
-
-            matrixBottomImages[currentIteration][j].style.transition = "transform 500ms ease-in-out";
-        }
-    }
-    setTimeout(() => {
-        spinSlots(matrixImages, matrixBottomImages, currentIteration - 1, flippedIteration + 1);
-    }, 500);
-
-}
-
-btn.onclick = () => {
-    const currentIteration = 2;
-    const flippedIteration = 1;
-    let currentIndex = 0;
-
+function FillSlotsMatrix() {
     const imges = document.querySelectorAll("[class=slots-image]");
     const bottomImges = document.querySelectorAll("[id=slotsFigure]");
 
@@ -44,12 +19,43 @@ btn.onclick = () => {
             currentIndex++;
         }
     }
+    return [matrixImages, matrixBottomImages];
+}
+
+function spinSlots(matrixImages, matrixBottomImages, currentIteration, flippedIteration) {
+
+    for (let i = 0; i < matrixImages.length - flippedIteration; i++) {
+        for (let j = 0; j < matrixImages[i].length; j++) {
+
+            matrixImages[i][j].style.transform = `translateY(${(180 * flippedIteration) + (8 * flippedIteration)}px)`;
+            matrixImages[i][j].style.transition = `transform ${spinLenght}ms ease-in-out`;
+
+            matrixBottomImages[currentIteration][j].style.transform = "translateY(180px)";
+            matrixBottomImages[currentIteration][j].style.transition = `transform ${spinLenght}ms ease-in-out`;
+        }
+    }
+
+    if (flippedIteration == matrixImages.length) {
+        for (let j = 0; j < matrixImages[0].length; j++) {
+            matrixBottomImages[currentIteration][j].style.transform = "translateY(180px)";
+
+            matrixBottomImages[currentIteration][j].style.transition = `transform ${spinLenght}ms ease-in-out`;
+        }
+    }
+    setTimeout(() => {
+        spinSlots(matrixImages, matrixBottomImages, currentIteration - 1, flippedIteration + 1);
+    }, spinLenght);
+
+}
+
+btn.onclick = () => {
+    btn.disabled = true;
 
     spinSlots(matrixImages, matrixBottomImages, currentIteration, flippedIteration);
 
     setTimeout(() => {
         document.getElementById("slotForm").submit();
-    }, 1600);
+    }, spinLenght * matrixImages[0].length);
 
     return false;
 }
