e4982702acb786dee4785271cbabe3b9ee676cdd
Diansavov <diansavov2@abv.bg>
2024-04-09 00:25:26 +0300

    ProfileLayout

---
+++ TheSchizoGamblers/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TheSchizoGamblers.Data;
+using TheSchizoGamblers.Models;
+
+namespace TheSchizoGamblers.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly GamblersContext _gamblersContext;
+        private readonly UserManager<GamblersModel> _userManager;
+
+        public ProfileController(UserManager<GamblersModel> userManager, GamblersContext gamblersContext)
+        {
+            this._userManager = userManager;
+            this._gamblersContext = gamblersContext;
+        }
+
+        [HttpGet]
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+        
+
+
+    }
+}
