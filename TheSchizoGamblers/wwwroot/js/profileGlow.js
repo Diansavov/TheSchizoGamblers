e9120d1ff5c79283f7807a35aa3c79f65857bdf6
Diansavov <diansavov2@abv.bg>
2024-04-10 17:01:42 +0300

    Profile Card Look

---
+++ TheSchizoGamblers/wwwroot/js/profileGlow.js
@@ -0,0 +1,10 @@
+document.getElementById("profiles").onmousemove = e => {
+    for (const card of document.getElementsByClassName("profile")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
