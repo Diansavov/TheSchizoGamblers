251ca538c1a82c9797695d813e52ff8ff6756e6b
Diansavov <diansavov2@abv.bg>
2024-05-15 20:18:06 +0300

    AlmostProfilePic

--- TheSchizoGamblers/wwwroot/js/registerForm/registerCropper.js
+++ TheSchizoGamblers/wwwroot/js/registerForm/registerCropper.js
@@ -7,14 +7,14 @@ function triggerFileUpload() {
 
 function handleFileUpload(event) {
   var file = event.target.files[0];
-  
+
   if (file) {
     var reader = new FileReader();
-    
+
     reader.onload = function (e) {
       var image = document.getElementById('imageCrop');
       image.src = e.target.result;
-      
+
       cropper.destroy();
       cropper = new Cropper(image, {
         aspectRatio: 1 / 1,
@@ -27,9 +27,18 @@ function handleFileUpload(event) {
         toggleDragModeOnDblclick: false,
         minContainerWidth: 466,
         minContainerHeight: 400,
-    });
+      });
     };
     reader.readAsDataURL(file);
+
+    const cropButton = document.getElementById('crop-btn');
+    cropButton.addEventListener('click', () => {
+      const croppedCanvas = cropper.getCroppedCanvas(); 
+      if (croppedCanvas) {
+        const croppedImage = croppedCanvas.toDataURL();
+        document.getElementById('real-profile-pic').src = croppedImage;
+      }
+    });
   }
 }
 
